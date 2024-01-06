fcf3e00a6f01d597302e3a3efaa9cdf5d1301ef8
Faisal Abdul Majid <95836280+Faisalabdulmajid@users.noreply.github.com>
2024-01-06 16:13:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-    <!DOCTYPE html>
-    <html lang="en">
-
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +7,6 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </head>
-
     <body>
         <div style="position: relative;">
             <img src="https://images.unsplash.com/photo-1603584173870-7f23fdae1b7a?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
@@ -18,5 +16,4 @@
                 <a href="masuk.php"><button type="button" class="btn btn-primary btn-lg"><b>MASUK</b></button></a>
             </div>
         </div>
-
-    </html>
+</html>
